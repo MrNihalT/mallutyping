302236f7b6b2aa97b7911a2770febaf6abf9ce68
MrNihalT <nihal.chiyoor@gmail.com>
2026-08-16 23:37:09 +0530

    vercal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import PublicHeader from "@/components/layout/PublicHeader";
 import LearningExperience from "@/components/typing/LearningExperience";
-
+import { Analytics } from "@vercel/analytics/next"
 export default function HomePage() {
     return (
         <div className="sky-wash paper-grid min-h-screen p-4">
             <div className="mx-auto flex max-w-[1500px] flex-col gap-4">
                 <PublicHeader />
                 <LearningExperience />
+                <Analytics />
             </div>
         </div>
     );
